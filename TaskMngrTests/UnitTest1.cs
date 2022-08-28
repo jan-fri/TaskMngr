4b675a9465192e4bcad357e44a5f984b5303e617
janfri <jan.frindt@webuyanycar.com>
2022-08-28 14:15:30 +0100

    create ASP .Net Core with Anguar

---
+++ TaskMngrTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TaskMngrTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
